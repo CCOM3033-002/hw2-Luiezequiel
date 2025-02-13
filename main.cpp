034f2641602f5484f29ee7fb61dfa8cbf0efcd26
Luiezequiel <luiezequiel.benitez@upr.edu>
2025-02-13 03:12:22 +0000

    jjjjj

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
+/*Luiezequiel Benitez Medero
+801247261
+*/
 #include<iostream>
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
-  
+  // Proposito programa
+  cout << "Este programa suma dos fracciones." << endl;
+
+  //Fracciones
+  int num1, denom1, num2, denom2;
+  char slash;
+  // Input
+  cout << "Entre las fracciones en el formato a/b." << endl;
+
+  //  Usuario entra valores
+  cout << "Entre la primera fracción: ";
+  cin >> num1 >> slash >> denom1;
+
+  cout << "Entre la segunda fracción: ";
+  cin >> num2 >> slash >> denom2;
+
+  // Calculos 
+  int num_result = num1 * denom2 + num2 * denom1;
+  int denom_result = denom1 * denom2;
+
+  //Resultado
+  cout << "La suma es " << num_result << slash << denom_result << endl;
+
+
   return 0;
 }
